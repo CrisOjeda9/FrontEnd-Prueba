09531281b1da7e408f8a568419b046f3d7d69181
“Cristian <210180@utxicotepec.edu.mx>
2024-10-16 11:03:12 -0600

    cambios

--- frontEnd/src/app/cita/cita.component.ts
+++ frontEnd/src/app/cita/cita.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SuscripcionComponent } from '../suscripcion/suscripcion.component';
 
 @Component({
   selector: 'app-cita',
   standalone: true,
-  imports: [],
+  imports: [SuscripcionComponent],
   templateUrl: './cita.component.html',
   styleUrl: './cita.component.css'
 })
