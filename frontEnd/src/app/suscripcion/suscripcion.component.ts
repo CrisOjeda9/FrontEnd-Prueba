6ec1fd1adb0bd1fe72b66220577652dce8e4546d
“Cristian <210180@utxicotepec.edu.mx>
2024-10-16 12:05:56 -0600

    cambios

--- frontEnd/src/app/suscripcion/suscripcion.component.ts
+++ frontEnd/src/app/suscripcion/suscripcion.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-suscripcion',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './suscripcion.component.html',
   styleUrl: './suscripcion.component.css'
 })
